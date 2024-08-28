c3e989f8e413c9a8c6a2d190a9a6b4041b95c6bd
Simone <lombardi.simone.unige@gmail.com>
2024-08-28 17:10:48 +0200

    before wipe

--- scripts/bug_as.py
+++ scripts/bug_as.py
@@ -1,4 +1,4 @@
-#! /usr/bin/env python
+#! /usr/bin/env python3
 
 import rospy
 from geometry_msgs.msg import Point, Pose, Twist
@@ -7,7 +7,7 @@ from nav_msgs.msg import Odometry
 import math
 import actionlib
 import actionlib.msg
-import assignment_2_2023.msg
+import assignment_2_2023.msg	
 from tf import transformations
 from std_srvs.srv import *
 import time
